40bbe65f1ca6c8cd2b6ddf451936a0b34de830fc
tanjii <51274569+tanjii@users.noreply.github.com>
2026-04-01 16:21:01 +0200

    Update setup.sh

--- linux/setup.sh
+++ linux/setup.sh
@@ -3,22 +3,25 @@
 echo "setup and configure server"
 
 fileName=config.yaml
-if [ -d "config" ]
+
+configDir=$1
+if [ -d "$configDir" ]
 then
         echo "reading config directory contents:"
-        configFiles=$(ls config)
+        configFiles=$(ls $configDir)
 else
         echo "config dir not found, creating one now"
-        mkdir config
+        mkdir "$configDir"
+        touch "$configDir/config.sh"
 fi
 
-userGroup=xx
-if [ userGroup == "admin"  ]
+userGroup=$2
+if [ "$userGroup" == "admin"  ]
 then
         echo "configure the server"
-elif [ userGroup == "parallels" ]
+elif [ "$userGroup" == "parallels" ]
 then "administer the server"
-else 
+else
         echo "no permission to configure the server. Wrong user group."
 fi
 
