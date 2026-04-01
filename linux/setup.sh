a553f7655b1297c99fc6c575d6f1109bc765f9f0
tanjii <51274569+tanjii@users.noreply.github.com>
2026-04-01 15:34:34 +0200

    Create setup.sh

---
+++ linux/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "setup and configure server"
+
+fileName=config.yaml
+configFiles=$(ls config)
+
+
+
+echo "using file $fileName to configure something"
+echo "here are all config files: $configFiles"
