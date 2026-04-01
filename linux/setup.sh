3cc3139650f781a7d8feba3628c4eb75cadfc3f7
tanjii <51274569+tanjii@users.noreply.github.com>
2026-04-01 15:44:40 +0200

    Update setup.sh

--- linux/setup.sh
+++ linux/setup.sh
@@ -3,9 +3,14 @@
 echo "setup and configure server"
 
 fileName=config.yaml
-configFiles=$(ls config)
-
-
+if [-d "config"]
+then
+        echo "reading config directory contents:"
+        configFiles=$(ls config)
+else
+        echo "config dir not found, creating one now"
+        mkdir config
+fi
 
 echo "using file $fileName to configure something"
 echo "here are all config files: $configFiles"
