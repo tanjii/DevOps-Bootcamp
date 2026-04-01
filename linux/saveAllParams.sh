3e39bf98b6776d395aa0275b8b71174982266c6c
tanjii <tanja.rsa@gmail.com>
2026-04-01 17:38:28 +0200

    added number of inputed params

--- linux/saveAllParams.sh
+++ linux/saveAllParams.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo "Reading user input"
+#stores all parametes user inputed in $* variable
 echo "All parameters user inputed: $*"
+
+#stores the number of parameters user inputed in $#
+echo "number of parameters user inputed: $#" 
 
 echo "user $1"
 echo "group $2"
